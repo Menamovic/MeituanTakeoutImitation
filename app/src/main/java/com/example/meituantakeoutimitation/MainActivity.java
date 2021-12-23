dd3b30a7369443692535637a5a493ab2c703d303
Menamovic <1362800827@qq.com>
2021-12-23 22:35:13 +0800

    '修改店铺列表界面，使得轮播广告和店铺列表一起滑动'

--- app/src/main/java/com/example/meituantakeoutimitation/MainActivity.java
+++ app/src/main/java/com/example/meituantakeoutimitation/MainActivity.java
@@ -32,10 +32,6 @@ public class MainActivity extends AppCompatActivity {
     Banner banner;
     //@BindView(R.id.indicator)
     RoundLinesIndicator indicator;
-//    @BindView(R.id.swipeRefresh)
-    //SwipeRefreshLayout refresh;
-
-    private ListView mListView;
     // 店铺图标
     private int[] icons={R.drawable.jd, R.drawable.qq, R.drawable.qq_dizhu
             , R.drawable.sina, R.drawable.tmall, R.drawable.uc};
@@ -62,90 +58,117 @@ public class MainActivity extends AppCompatActivity {
 
         banner = (Banner) findViewById(R.id.banner);
         indicator = (RoundLinesIndicator) findViewById(R.id.indicator);
-        //refresh = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh);
 
-        //自定义的图片适配器，也可以使用默认的BannerImageAdapter
+        //自定义图片适配器
         ImageAdapter adapter = new ImageAdapter(DataBean.getTestData());
 
+        // 配置轮播广告
         banner.setAdapter(adapter)
-//              .setCurrentItem(0,false)
                 .addBannerLifecycleObserver(this)//添加生命周期观察者
                 .setIndicator(new CircleIndicator(this))//设置指示器
                 .setOnBannerListener((data, position) -> {
                     Snackbar.make(banner, ((DataBean) data).title, Snackbar.LENGTH_SHORT).show();
                     LogUtils.d("position：" + position);
                 });
-/*
-        //和下拉刷新配套使用
-        refresh.setOnRefreshListener(() -> {
-            //模拟网络请求需要3秒，请求完成，设置setRefreshing 为false
-            new Handler().postDelayed(() -> {
-                refresh.setRefreshing(false);
-
-                //给banner重新设置数据
-                banner.setDatas(DataBean.getTestData());
-
-                //对setDatas()方法不满意？你可以自己在adapter控制数据，参考setDatas()的实现修改
-//                adapter.updateData(DataBean.getTestData());
-//                banner.setCurrentItem(banner.getStartPosition(), false);
-//                banner.setIndicatorPageChange();
-
-            }, 3000);
-        });
-
-        refresh.setEnabled(true);
-*/
         banner.setAdapter(new ImageAdapter(DataBean.getTestData()));
         banner.setIndicator(new CircleIndicator(this));
         banner.setIndicatorGravity(IndicatorConfig.Direction.CENTER);
 
-        mListView=(ListView)findViewById(R.id.shopList);
-        mListView.setAdapter(new MyBaseAdapter());
+        // 加载店铺信息
+        View shop1 = (View) findViewById(R.id.shop1);
+        ImageButton shop1_ib = (ImageButton) shop1.findViewById(R.id.shop_image_button);
+        shop1_ib.setBackgroundResource(icons[0]);
+        Button shop1_name = (Button) shop1.findViewById(R.id.shop_name_button);
+        shop1_name.setText(names[0]);
+        Button shop1_sellNum = (Button) shop1.findViewById(R.id.sell_num_button);
+        shop1_sellNum.setText(Integer.toString(sellNums[0]));
+        Button shop1_averagePrice = (Button) shop1.findViewById(R.id.average_price_button);
+        shop1_averagePrice.setText(Double.toString(averagePrices[0]));
+        Button shop1_startSendingPrice = (Button) shop1.findViewById(R.id.start_sending_price_button);
+        shop1_startSendingPrice.setText(Double.toString(startSendingPrices[0]));
+        Button shop1_sendingPrice = (Button) shop1.findViewById(R.id.sending_price_button);
+        shop1_sendingPrice.setText(Double.toString(sendingPrices[0]));
+        TextView shop1_characteristic = (TextView) shop1.findViewById(R.id.tv_characteristic);
+        shop1_characteristic.setText(characteristic[0]);
+
+        View shop2 = (View) findViewById(R.id.shop2);
+        ImageButton shop2_ib = (ImageButton) shop2.findViewById(R.id.shop_image_button);
+        shop2_ib.setBackgroundResource(icons[1]);
+        Button shop2_name = (Button) shop2.findViewById(R.id.shop_name_button);
+        shop2_name.setText(names[1]);
+        Button shop2_sellNum = (Button) shop2.findViewById(R.id.sell_num_button);
+        shop2_sellNum.setText(Integer.toString(sellNums[1]));
+        Button shop2_averagePrice = (Button) shop2.findViewById(R.id.average_price_button);
+        shop2_averagePrice.setText(Double.toString(averagePrices[1]));
+        Button shop2_startSendingPrice = (Button) shop2.findViewById(R.id.start_sending_price_button);
+        shop2_startSendingPrice.setText(Double.toString(startSendingPrices[1]));
+        Button shop2_sendingPrice = (Button) shop2.findViewById(R.id.sending_price_button);
+        shop2_sendingPrice.setText(Double.toString(sendingPrices[1]));
+        TextView shop2_characteristic = (TextView) shop2.findViewById(R.id.tv_characteristic);
+        shop2_characteristic.setText(characteristic[1]);
+
+        View shop3 = (View) findViewById(R.id.shop3);
+        ImageButton shop3_ib = (ImageButton) shop3.findViewById(R.id.shop_image_button);
+        shop3_ib.setBackgroundResource(icons[2]);
+        Button shop3_name = (Button) shop3.findViewById(R.id.shop_name_button);
+        shop3_name.setText(names[2]);
+        Button shop3_sellNum = (Button) shop3.findViewById(R.id.sell_num_button);
+        shop3_sellNum.setText(Integer.toString(sellNums[2]));
+        Button shop3_averagePrice = (Button) shop3.findViewById(R.id.average_price_button);
+        shop3_averagePrice.setText(Double.toString(averagePrices[2]));
+        Button shop3_startSendingPrice = (Button) shop3.findViewById(R.id.start_sending_price_button);
+        shop3_startSendingPrice.setText(Double.toString(startSendingPrices[2]));
+        Button shop3_sendingPrice = (Button) shop3.findViewById(R.id.sending_price_button);
+        shop3_sendingPrice.setText(Double.toString(sendingPrices[2]));
+        TextView shop3_characteristic = (TextView) shop3.findViewById(R.id.tv_characteristic);
+        shop3_characteristic.setText(characteristic[2]);
+
+        View shop4 = (View) findViewById(R.id.shop4);
+        ImageButton shop4_ib = (ImageButton) shop4.findViewById(R.id.shop_image_button);
+        shop4_ib.setBackgroundResource(icons[3]);
+        Button shop4_name = (Button) shop4.findViewById(R.id.shop_name_button);
+        shop4_name.setText(names[3]);
+        Button shop4_sellNum = (Button) shop4.findViewById(R.id.sell_num_button);
+        shop4_sellNum.setText(Integer.toString(sellNums[3]));
+        Button shop4_averagePrice = (Button) shop4.findViewById(R.id.average_price_button);
+        shop4_averagePrice.setText(Double.toString(averagePrices[3]));
+        Button shop4_startSendingPrice = (Button) shop4.findViewById(R.id.start_sending_price_button);
+        shop4_startSendingPrice.setText(Double.toString(startSendingPrices[3]));
+        Button shop4_sendingPrice = (Button) shop4.findViewById(R.id.sending_price_button);
+        shop4_sendingPrice.setText(Double.toString(sendingPrices[3]));
+        TextView shop4_characteristic = (TextView) shop4.findViewById(R.id.tv_characteristic);
+        shop4_characteristic.setText(characteristic[3]);
+
+        View shop5 = (View) findViewById(R.id.shop5);
+        ImageButton shop5_ib = (ImageButton) shop5.findViewById(R.id.shop_image_button);
+        shop5_ib.setBackgroundResource(icons[4]);
+        Button shop5_name = (Button) shop5.findViewById(R.id.shop_name_button);
+        shop5_name.setText(names[4]);
+        Button shop5_sellNum = (Button) shop5.findViewById(R.id.sell_num_button);
+        shop5_sellNum.setText(Integer.toString(sellNums[4]));
+        Button shop5_averagePrice = (Button) shop5.findViewById(R.id.average_price_button);
+        shop5_averagePrice.setText(Double.toString(averagePrices[4]));
+        Button shop5_startSendingPrice = (Button) shop5.findViewById(R.id.start_sending_price_button);
+        shop5_startSendingPrice.setText(Double.toString(startSendingPrices[4]));
+        Button shop5_sendingPrice = (Button) shop5.findViewById(R.id.sending_price_button);
+        shop5_sendingPrice.setText(Double.toString(sendingPrices[4]));
+        TextView shop5_characteristic = (TextView) shop5.findViewById(R.id.tv_characteristic);
+        shop5_characteristic.setText(characteristic[4]);
+
+        View shop6 = (View) findViewById(R.id.shop6);
+        ImageButton shop6_ib = (ImageButton) shop6.findViewById(R.id.shop_image_button);
+        shop6_ib.setBackgroundResource(icons[5]);
+        Button shop6_name = (Button) shop6.findViewById(R.id.shop_name_button);
+        shop6_name.setText(names[5]);
+        Button shop6_sellNum = (Button) shop6.findViewById(R.id.sell_num_button);
+        shop6_sellNum.setText(Integer.toString(sellNums[5]));
+        Button shop6_averagePrice = (Button) shop6.findViewById(R.id.average_price_button);
+        shop6_averagePrice.setText(Double.toString(averagePrices[5]));
+        Button shop6_startSendingPrice = (Button) shop6.findViewById(R.id.start_sending_price_button);
+        shop6_startSendingPrice.setText(Double.toString(startSendingPrices[5]));
+        Button shop6_sendingPrice = (Button) shop6.findViewById(R.id.sending_price_button);
+        shop6_sendingPrice.setText(Double.toString(sendingPrices[5]));
+        TextView shop6_characteristic = (TextView) shop6.findViewById(R.id.tv_characteristic);
+        shop6_characteristic.setText(characteristic[5]);
     }
-
-    class MyBaseAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            return names.length;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return names [position];
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view=View.inflate(MainActivity.this
-                                    , R.layout.shoplist_item, null);
-//            TextView mTextView=(TextView) view.findViewById(R.id.tv_list);
-//            ImageView imageView=(ImageView)view.findViewById(R.id.image);
-            ImageButton imageButton = (ImageButton) view.findViewById(R.id.shop_image_button);
-            Button btnName = (Button) view.findViewById(R.id.shop_name_button);
-            Button btnSellNum = (Button) view.findViewById(R.id.sell_num_button);
-            Button btnAveragePrice = (Button) view.findViewById(R.id.average_price_button);
-            Button btnStartSendingPrice = (Button) view.findViewById(R.id.start_sending_price);
-            Button btnSendingPrice = (Button) view.findViewById(R.id.sending_price);
-            TextView tvCharacteristic = (TextView) view.findViewById(R.id.tv_characteristic);
-            // 组装数据
-//            mTextView.setText(names[position]);
-//            imageView.setBackgroundResource(icons[position]);
-            imageButton.setBackgroundResource(icons[position]);
-            btnName.setText(names[position]);
-            btnSellNum.setText(Integer.toString(sellNums[position]));
-            btnAveragePrice.setText(Double.toString(averagePrices[position]));
-            btnStartSendingPrice.setText(Double.toString(startSendingPrices[position]));
-            btnSendingPrice.setText(Double.toString(sendingPrices[position]));
-            tvCharacteristic.setText(characteristic[position]);
-            // 组装完成后返回
-            return view;
-        }
-    }
-
 }
